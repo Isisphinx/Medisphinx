5e6e6cae33c187972d3789a2344709f5aeb2a318
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2020-12-14 23:37:42 +0100

    fix muilist name

--- src/components/myList.js
+++ src/components/myList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import List from '@material-ui/core/List'
-import ListItem2Patch from '@material-ui/core/ListItem'
+import MuiListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import { useSelector } from 'react-redux'
 import patchBaseButtonComponent from 'gatsby-material-ui-components/lib/patch-base-button-components'
 
-const ListItem = patchBaseButtonComponent(ListItem2Patch)
+const ListItem = patchBaseButtonComponent(MuiListItem)
 // Need to patch List button to use gatsby link
 
 const MyList = () => {
