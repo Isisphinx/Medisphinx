5f72c14bf1b127745aa53caf32b6bdf1d30ee73a
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-15 15:23:31 +0100

    {*} add textfield

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import MyList from '../components/myList'
+import TextField from '@material-ui/core/TextField';
 
-const Home = () => <MyList />
+const Home = () => <div>
+    <TextField id="standard-basic" label="Standard" />
+    <MyList />
+</div>
 export default Home
