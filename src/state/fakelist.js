21fb04735b31b03c111d3e44a2ace978172e5189
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-15 11:24:14 +0100

    {*} separate name in firstname and lastname

--- src/state/fakelist.js
+++ src/state/fakelist.js
@@ -4,8 +4,8 @@ import { useDispatch } from 'react-redux'
 
 const SetFakeList = () => {
   const dispatch = useDispatch()
-  const createUser = () => ({ uuid: faker.random.uuid(), email: faker.internet.email(), address: faker.address.streetAddress(), bio: faker.lorem.sentence(), image: faker.image.avatar(), name: faker.name.findName(),
-                              age: faker.random.number({'min': 0, 'max':80})})
+  const createUser = () => ({ uuid: faker.random.uuid(), email: faker.internet.email(), address: faker.address.streetAddress(), bio: faker.lorem.sentence(), image: faker.image.avatar(), firstname: faker.name.firstName(),
+                              lastname: faker.name.lastName(), age: faker.random.number({'min': 0, 'max':80})})
 
   const createUsers = (numUsers = 5) => Array.from({ length: numUsers }, createUser)
   const Users = createUsers()
