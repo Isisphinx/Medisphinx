697b525a277390a722feac41ad796b4a973d5435
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-16 17:21:59 +0100

    {*} add seach icon in TextField

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import MyList from '../components/myList'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Home = () =>
   <div>
-    <TextField id="standard-basic" label="Standard" />
+    <TextField id="standard-basic" label={<SearchIcon/>} />
     <Button variant="outlined">All</Button>
     <Button variant="outlined">To do</Button>
     <MyList/>
