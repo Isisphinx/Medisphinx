db02a0e976f7ebc25f9d7c9d84b952b334259c0b
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-16 17:13:13 +0100

    {*} add button all and to do in indenx.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import MyList from '../components/myList'
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const Home = () =>
   <div>
     <TextField id="standard-basic" label="Standard" />
+    <Button variant="outlined">All</Button>
+    <Button variant="outlined">To do</Button>
     <MyList/>
   </div>
 export default Home
