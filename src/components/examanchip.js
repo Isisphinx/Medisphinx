fa7d28a75329f7259a18ffaa4123b42012309308
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-16 09:27:30 +0100

    {*} examanchip3js

--- src/components/examanchip.js
+++ src/components/examanchip.js
@@ -1,64 +1,26 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Chip from '@material-ui/core/Chip';
-import DoneIcon from '@material-ui/icons/Done';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'left',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(0.5),
-    },
-  },
-}));
+import React from 'react'
+import Chip from '@material-ui/core/Chip'
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max))
 }
 
 export default function Chips() {
-  const classes = useStyles();
-
-  const handleDelete = () => {
-    console.info('You clicked the delete icon.');
-  };
-
   const nb = getRandomInt(2)
 
   console.log(nb)
 
-  const handleClick = () => {
-    console.info('You clicked the Chip.');
-  };
-
-if (nb === 0)
-  return (
-    <div className={classes.root}>
-      <Chip
-        label="radio Genou"
-        onClick={handleClick}
-        onDelete={handleDelete}
-        deleteIcon={<DoneIcon />}
-      />
-    </div>
-  );
-    else if (nb > 0)
-      return (    <div className={classes.root}>
-        <Chip
-          label="radio Genou"
-          onClick={handleClick}
-          onDelete={handleDelete}
-          deleteIcon={<DoneIcon />}
-        />
-          <Chip
-          label="Echo genou"
-          onClick={handleClick}
-          onDelete={handleDelete}
-          deleteIcon={<DoneIcon />}
-        />
+  if (nb === 0)
+    return (
+      <div>
+        <Chip label='radio Genou' />
       </div>
-    );
-
+    )
+  else if (nb > 0)
+    return (
+      <div>
+        <Chip label='radio Genou' />
+        <Chip label='Echo genou' />
+      </div>
+    )
 }
