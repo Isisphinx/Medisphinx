65c2ae214c10b86f98d2ce8515d90b3540a7c1f1
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-16 16:12:14 +0100

    {*} add create button in myList

--- src/components/myList.js
+++ src/components/myList.js
@@ -5,6 +5,8 @@ import ListItemText from '@material-ui/core/ListItemText'
 import { useSelector } from 'react-redux'
 import patchBaseButtonComponent from 'gatsby-material-ui-components/lib/patch-base-button-components'
 import Chips from './examanchip'
+import CreateIcon from '@material-ui/icons/Create';
+import IconButton from '@material-ui/core/IconButton';
 
 const ListItem = patchBaseButtonComponent(MuiListItem)
 // Need to patch List button to use gatsby link
@@ -13,7 +15,10 @@ const MyList = () => {
   const Users = useSelector((state) => state.fakelist.value)
   const name = Users.map((user) => (
     <ListItem button key={user.uuid}>
-      <ListItemText primary={`${user.lastname} ${user.firstname} ${user.age} ans ${user.lastname.substring(0, 3)}`} secondary={<Chips/>}/>
+      <ListItemText primary={`${user.lastname} ${user.firstname} ${user.age} ans ${user.lastname.substring(0, 3)}`  } secondary={<Chips/>}/>
+      <IconButton>
+        <CreateIcon />
+      </IconButton>
     </ListItem>
   ))
 
