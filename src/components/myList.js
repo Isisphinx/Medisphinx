98747fe1094a370074a8345b8007882261f02241
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-17 10:02:56 +0100

    {*} add ListItemSecondaryAction

--- src/components/myList.js
+++ src/components/myList.js
@@ -5,8 +5,9 @@ import ListItemText from '@material-ui/core/ListItemText'
 import { useSelector } from 'react-redux'
 import patchBaseButtonComponent from 'gatsby-material-ui-components/lib/patch-base-button-components'
 import Chips from './examanchip'
-import CreateIcon from '@material-ui/icons/Create';
-import IconButton from '@material-ui/core/IconButton';
+import CreateIcon from '@material-ui/icons/Create'
+import IconButton from '@material-ui/core/IconButton'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 
 const ListItem = patchBaseButtonComponent(MuiListItem)
 // Need to patch List button to use gatsby link
@@ -15,11 +16,13 @@ const MyList = () => {
   const Users = useSelector((state) => state.fakelist.value)
   const name = Users.map((user) => (
     <ListItem button key={user.uuid}>
-      <ListItemText primary={`${user.lastname} ${user.firstname} ${user.age} ans`  } secondary={<Chips/>}/>
+      <ListItemText primary={`${user.lastname} ${user.firstname} ${user.age} ans`} secondary={<Chips />} />
       {user.lastname.substring(0, 3)}
-      <IconButton>
-        <CreateIcon />
-      </IconButton>
+      <ListItemSecondaryAction>
+        <IconButton>
+          <CreateIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   ))
 
