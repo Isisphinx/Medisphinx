1162cc7db595bf92eefb2bb7b8faf97164c3fe77
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-17 14:46:17 +0100

    {*} add ToggleButton

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormControl from '@material-ui/core/FormControl'
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
+import React from 'react';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
-const RadioButtonsGroup = () => {
-  const [value, setValue] = React.useState('female')
+export default function StandaloneToggleButton() {
+  const [selected, setSelected] = React.useState(false);
+  const [todo, settodo] = React.useState(false);
 
-  const handleChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return (
-    <FormControl component='fieldset'>
-      <RadioGroup name='To Do' value={value} onChange={handleChange}>
-        <FormControlLabel value='All' control={<Radio />} label='All' />
-        <FormControlLabel value='To Do' control={<Radio />} label='To Do' />
-      </RadioGroup>
-    </FormControl>
+  return ( <div>
+    <ToggleButton
+        value="check"
+      selected={selected}
+      onChange={() => {
+        setSelected(!selected);
+      }}>
+      All
+    </ToggleButton>
+    <ToggleButton
+            value="check"
+            selected={todo}
+            onChange={() => {
+              settodo(!todo);
+            }}>
+        To Do
+    </ToggleButton></div>
   )
 }
-
-export default RadioButtonsGroup
