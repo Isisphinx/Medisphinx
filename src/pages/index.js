63772c94d808fd3d39e1728c647e8539cba0f694
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-17 09:26:16 +0100

    {*} fix problem with seach icon in textfield

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,21 @@ import MyList from '../components/myList'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
 
 const Home = () =>
   <div>
-    <TextField id="standard-basic" label={<SearchIcon/>} />
+    <TextField
+        id="input-with-icon-textfield"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
     <Button variant="outlined">All</Button>
     <Button variant="outlined">To do</Button>
     <MyList/>
