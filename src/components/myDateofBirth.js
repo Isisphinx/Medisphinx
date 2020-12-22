6da900327c3798391720c8d2c899cec2c8539da8
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-22 16:53:35 +0100

    {+} myDateofBirth.js

---
+++ src/components/myDateofBirth.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+
+
+const myDateofBirh = () => {
+    return (
+    <div>
+        <TextField
+        id='input-with-icon-textfield'
+        />
+    </div>
+    )
+}
+
+export default myDateofBirh
