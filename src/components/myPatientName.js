5137cfbb9853b86a6ec674f5ede9a14f36dc4ffe
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-23 16:26:10 +0100

    {*} add iconeButton add

--- src/components/myPatientName.js
+++ src/components/myPatientName.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import MyPatientNameList from './myPatientNameList'
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
 
 const MyPatientName = () => {
 
@@ -8,6 +10,9 @@ const MyPatientName = () => {
     return (<div> 
         <TextField id='name-field' label='prénom'/>
         <TextField id='lastname-field' label='nom'/>
+        <IconButton>
+            <AddIcon/>
+        </IconButton>
         <MyPatientNameList/>
     </div>)
 }
