bfb1efc26023a2d7f73745a91c95232160c2d9d8
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-29 17:02:53 +0100

    {*} move switch in index.js

--- src/components/mySwitch.js
+++ src/components/mySwitch.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import Switch from '@material-ui/core/Switch';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    marginLeft: theme.spacing(1),
+  },
+}));
 
 export default function CustomizedSwitches() {
+  const classes = useStyles();
     const [state, setState] = React.useState({
       checkedC: true,
     })
@@ -9,8 +18,10 @@ export default function CustomizedSwitches() {
     setState({ ...state, [event.target.name]: event.target.checked });
   }
 
-    return (<div>
+    return (<div className={classes.root}>
+            all
             <Switch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+            to do
         </div>
     )
 }
