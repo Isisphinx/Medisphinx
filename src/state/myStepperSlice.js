60b410546b974fc3aadda76a588e5f089b843fa7
De Nascimento <quentin.de-nascimento@epitech.eu>
2020-12-31 11:26:13 +0100

    {+} myStepperSlice

---
+++ src/state/myStepperSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {}
+
+const myStepperListSlice = createSlice({
+  name: 'fakeliste',
+  initialState,
+  reducers: {
+  },
+})
+
+export const { } = myStepperListSlice.actions
+export default myStepperListSlice.reducer
